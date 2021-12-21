d555b65abd739d3323f7db3503b84f14f9330485
rohit <rohitmaity13112002@gmail.com>
2021-12-21 21:53:58 +0530

    Add files via upload

---
+++ casual adding.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num1,num2,result;
+	cout<<"please enter first number\n";
+	cin>>num1;
+	cout<<"please enter second number\n";
+	cin>>num2;
+	result=num1+num2;
+	cout<<"sum of two number\n"<<result;
+	
+	return 0;
+}
